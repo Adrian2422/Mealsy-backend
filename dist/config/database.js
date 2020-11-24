2c1fed6cf13ac9d0e87ad571d209a5c0a6cc904e
AK <adrian.kloc47@gmail.com>
2020-11-25 00:12:47 +0100

    heroku deployment fix #1

--- dist/config/database.js
+++
@@ -1,3 +0,0 @@
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.default = {
-    mongoUrl: 'mongodb://127.0.0.1:27017/mealsy' };
-//# sourceMappingURL=database.js.map
