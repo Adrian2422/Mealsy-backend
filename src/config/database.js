64b6544219be8e5fb8a15e90a45bfcd005981808
AK <adrian.kloc47@gmail.com>
2020-11-25 10:22:13 +0100

    heroku deployment fix #7

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,3 @@
 export default {
-    mongoUrl: 'mongodb+srv://AdrianKloc47:P0klik4$@cluster0.oppqy.mongodb.net/mealsy?retryWrites=true&w=majority'
+    mongoUrl: 'mongodb+srv://AdrianKloc47:P0klik4%24@cluster0.oppqy.mongodb.net/mealsy?retryWrites=true&w=majority'
 }
