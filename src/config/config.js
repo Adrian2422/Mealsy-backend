7a12187884258aca0b29d1ba51459f232f3b5a50
AK <adrian.kloc47@gmail.com>
2020-11-25 11:17:07 +0100

    heroku deployment fix #13

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 // required environment variables
-// ['NODE_ENV', 'PORT'].forEach((name) => {
-//     if (!process.env[name]) {
-//         throw new Error(`Environment variable ${name} is missing`)
-//     }
-// });
+['NODE_ENV', 'PORT'].forEach((name) => {
+    if (!process.env[name]) {
+        throw new Error(`Environment variable ${name} is missing`)
+    }
+});
 
 export default  {
     env: process.env.NODE_ENV,
